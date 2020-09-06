30ada82c23d463f32e886e0eca20923c34610ed1
Jason Ph <vinhphuong0206@gmail.com>
2020-09-06 22:19:08 +1000

    first commit

---
+++ ContosoUniversityModelBinding/Students.aspx.cs
@@ -0,0 +1,23 @@
+﻿using ContosoUniversityModelBinding.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+
+namespace ContosoUniversityModelBinding
+{
+    public partial class Students : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public IQueryable<Student> studentsGrid_GetData()
+        {
+            SchoolContext db = new SchoolContext();
+            var query = db.Students.Include(s => s.Enrollments.Select(e => e.Course));
+            return query;
+        }
+    }
+}
